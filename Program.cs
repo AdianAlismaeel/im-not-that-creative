2a9a9e53f3b210c6917ad1cc902df913360d7a90
AdianAlismaeel <adal04001@utb.vaxjo.se>
2021-09-13 10:11:05 +0200

    skapat projekt

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace im_not_that_creative
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
