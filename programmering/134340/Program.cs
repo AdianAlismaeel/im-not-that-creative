79bd72f8dacbb0e41c1b5694f514f7f82be60eb3
AdianAlismaeel <adal04001@utb.vaxjo.se>
2021-09-20 08:12:12 +0200

    Jag har lagt till projektet.

---
+++ programmering/134340/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace _134340
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int datortal = rnd.Next(1,101);
+
+            bool vinst = false;  
+                                
+
+            int gissningar = 0;
+            
+
+            do                  
+            {                   
+                Console.WriteLine("Gissa på ett nummer mellan 1 och 100: ");
+                string svar = Console.ReadLine();
+                int tal = int.Parse(svar);
+
+                if (tal > datortal)
+                  {
+                    Console.WriteLine("Talet är för högt! Försök igen...");
+                  }
+
+                else if (tal < datortal)
+                    {
+                        Console.WriteLine("Talet är för lågt! Försök igen...");
+                    }
+
+                else if (tal == datortal)
+                    {
+                        Console.WriteLine("Du gissade rätt!");
+                        vinst = true;  
+                    }
+               
+                 gissningar++; 
+            }while (!vinst);
+            Console.WriteLine("Det tog dig " + gissningar++ + " försök");
+            
+        }
+        
+    }
+}
